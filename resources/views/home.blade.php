52aebc2d2fb655e48480174841a8519d2543fe8c
ClaudioScordari <scordari22@gmail.com>
2024-02-22 15:55:07 +0100

    first commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Laravel Primi Passi</title>
+</head>
+
+<body>
+    <main>
+        <h1>Primi passi con Laravel</h1>
+        <h2>Speriamo vada tutto bene</h2>
+    </main>
+</body>
+
+</html>
